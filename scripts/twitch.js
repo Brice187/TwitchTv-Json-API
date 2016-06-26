545a3383f83afaf445a9e57ae463c97c0e122eeb
Brice187 <lars@kerick.net>
2016-06-26 22:30:51 +0200

    updated functionality, still lots of work to do

--- scripts/twitch.js
+++ scripts/twitch.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
+	$.getJSON('https://api.twitch.tv/kraken/streams', function (data) {
+		var items = [];
+		for (var i = 0; i < data.streams.length; i++) {
+			$.each(data.streams[i].channel, function (key, val) {
+				if (key === "display_name")
+					items.push("<div class =\"row\"><div class=\"entry\"><a href=\"" + data.streams[i].channel.url + "\">" + val + "</a><img src=\"" + data.streams[i].preview.medium + "</img></div></div>");
+			});
+		};
 
+
+		$("<p>", {
+			"class": "my-new-list",
+			html: items.join("")
+		}).appendTo("#channels");
+	});
 });
